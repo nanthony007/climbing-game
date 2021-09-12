ec434cccc8b0885283b9beefb84c32f81150c284
Nick Anthony <nanthony007@gmail.com>
2021-09-11 20:20:05 -0400

    Implement simple POST route.

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/nanthony007/climbing-game/pkg"
+	"github.com/nanthony007/climbing-game/pkg/models"
+	"log"
 )
+
+// This is a pattern for API use
+//oldPoints := i.TotalPoints()
+//i.Routes = append(i.Routes, route)
+//newPoints := i.TotalPoints()
 
 func main() {
 	r := gin.Default()
@@ -12,11 +19,19 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.GET("/grade", func(c *gin.Context) {
-		input := pkg.Input{Grade: "v5", Seconds: 400.25}
-		output, _ := input.Compute()
-		c.JSON(200, gin.H{
-			"output": output,
+	r.POST("/problem", func(c *gin.Context) {
+		var input models.Input
+		// If `GET`, only `Form` binding engine (`query`) used.
+		// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
+		// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L48
+		err := c.ShouldBind(&input)
+		if err != nil {
+			log.Fatal("Could not assemble data into json.")
+		}
+		newData := input.Compute()
+		fmt.Println(newData)
+		c.JSON(201, gin.H{
+			"output": newData,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
